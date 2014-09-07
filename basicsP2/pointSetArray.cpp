b3f350963d713301373d38768938784d9c86a230
shixudongleo <shixudongleo@gmail.com>
2014-09-07 18:05:50 +0800

    implement functions in PointSetArray

--- basicsP2/pointSetArray.cpp
+++ basicsP2/pointSetArray.cpp
@@ -1,1 +1,19 @@
 #include "pointSetArray.h"
+
+int PointSetArray::getPoint (int pIndex, LongInt& x1,LongInt& y1) // put the x,y values into x1,y1, and return 1 if the point pIndex exists
+{
+	struct MyPoint point = myPoints.at(pIndex);
+	x1 = point.x;
+	y1 = point.y;
+
+	return 1;
+}
+
+int PointSetArray::noPt()                                        // return the number of points
+{
+	return myPoints.size();
+}
+void PointSetArray::eraseAllPoints()                            // erase all points in this array
+{
+	myPoints.clear();
+}
